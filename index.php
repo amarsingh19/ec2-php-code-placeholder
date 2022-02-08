33c317b8530753bf3bddb0052e2d6c14ca226c5c
Amarjit Chana <amar.net@gmail.com>
2022-02-07 19:15:41 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <h1>Hello EC2!</h1>
 <h5><b>Hello from EC2</b></h5>
 <?php
-   $userName="EC2 Test User"  ;
+   $userName="EC2 Test User1"  ;
    echo "<br>";
-   echo "Hello {$userName}";
+   echo "Hello {$userName}, how are you?";
 ?>
 </body>
 </html>
